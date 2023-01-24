c397c84d47df8cc11c87d5d678ecf14a39c24809
KGESH <seonghyeon.jee@gmail.com>
2023-01-24 17:22:49 +0900

    Refactor: cleanup demo code

--- src/main/java/qraps/platform/review/service/ExpertSystemReviewService.java
+++ src/main/java/qraps/platform/review/service/ExpertSystemReviewService.java
@@ -1,50 +1,17 @@
 package qraps.platform.review.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-import qraps.platform.report.service.ReportService;
-import qraps.platform.review.dto.RequestExpertSystemDto;
 import qraps.platform.review.dto.ResponseReviewDto;
 import qraps.platform.web.controller.dto.ReviewPageDto;
 
 @Service
 public class ExpertSystemReviewService {
 
-    private final CsvParserService csvParserService;
-
-    private final ReportService reportService;
-
-    @Autowired
-    public ExpertSystemReviewService(CsvParserService csvParserService, ReportService reportService) {
-        this.csvParserService = csvParserService;
-        this.reportService = reportService;
-    }
-
-
-    /**
-     * Todo: replace after demo
-     */
-    private ResponseReviewDto getReviewResultFromExpertSystem(String target, MultipartFile uploadedFile) {
-        RequestExpertSystemDto requestBody = RequestExpertSystemDto.builder()
-                .target(target)
-                .file(uploadedFile)
-                .build();
-
-        String url = "http://expert:5001/verify";
-        WebClient apiClient = WebClient.builder().baseUrl(url).build();
-        return apiClient
-                .post()
-                .uri(url)
-                .bodyValue(requestBody)
-                .retrieve()
-                .bodyToMono(ResponseReviewDto.class)
-                .block();
-    }
 
     /**
      * Expert System에
