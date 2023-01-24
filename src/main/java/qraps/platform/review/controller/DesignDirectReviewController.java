ae1d6e28ae6a3512685b29d5000a53fb6d788891
KGESH <seonghyeon.jee@gmail.com>
2023-01-24 16:52:26 +0900

    Feat: 플랫폼 직접 검증

---
+++ src/main/java/qraps/platform/review/controller/DesignDirectReviewController.java
@@ -0,0 +1,97 @@
+package qraps.platform.review.controller;
+
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import qraps.platform.review.dto.ResponseReviewDto;
+import qraps.platform.review.service.DirectReviewService;
+import qraps.platform.review.service.ValidationService;
+import qraps.platform.web.controller.dto.ReviewPageDto;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+
+@Tag(name = "DirectReview", description = "플랫폼을 통한 직접 검증 API")
+@Controller
+public class DesignDirectReviewController {
+
+    private final DirectReviewService designReviewService;
+    private TemplateEngine templateEngine;
+
+    @Autowired
+    public DesignDirectReviewController(DirectReviewService designReviewService, ValidationService validationService, TemplateEngine templateEngine) {
+        this.designReviewService = designReviewService;
+        this.templateEngine = templateEngine;
+    }
+
+
+    @PostMapping(path = "review/direct", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String review(@ModelAttribute("reviewDto") ReviewPageDto reviewDto, @RequestParam("file_input") MultipartFile uploadedFile, HttpServletRequest request) throws Exception {
+        ResponseReviewDto reviewResult = designReviewService.directReview(reviewDto.getValidTarget(), uploadedFile);
+
+        HttpSession session = request.getSession();
+        session.setAttribute("reviewResult", reviewResult);
+
+        return "redirect:/validation_result";
+    }
+
+
+    /**
+     * Todo: remove
+     */
+    @GetMapping("jasper/pdf")
+    @ResponseBody
+    public ResponseEntity<byte[]> generateJasperPdf() throws JRException {
+
+
+        Context context = new Context();
+
+//        InputStream templatePath = this.getClass().getResourceAsStream("/static/example.jrxml");
+        String templateName = "example";
+        String renderedHtmlContent = templateEngine.process(templateName, context);
+        InputStream inputStream = new ByteArrayInputStream(renderedHtmlContent.getBytes());
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, new HashMap<>());
+
+        JRPdfExporter exporter = new JRPdfExporter();
+        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outputStream));
+        SimplePdfExporterConfiguration configuration = new SimplePdfExporterConfiguration();
+        configuration.setCreatingBatchModeBookmarks(true);
+        exporter.setConfiguration(configuration);
+        exporter.exportReport();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_PDF_VALUE);
+        String fileName = "example" + "_report.pdf";
+        headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"");
+
+
+        byte[] pdfBytes = outputStream.toByteArray();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .headers(headers)
+                .body(pdfBytes);
+    }
+
+}
--- src/main/java/qraps/platform/review/service/DirectReviewService.java
+++ src/main/java/qraps/platform/review/service/DirectReviewService.java
@@ -3,65 +3,109 @@ package qraps.platform.review.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import qraps.platform.report.dto.ReportDto;
-import qraps.platform.report.service.ReportService;
+import qraps.platform.review.dto.ExcelMapperDto;
+import qraps.platform.review.dto.ResponseReviewDto;
 import qraps.platform.review.dto.ReviewDto;
-import qraps.platform.review.entity.DevMockEntity;
-import qraps.platform.review.repository.DevMockEntityRepository;
+import qraps.platform.review.repository.DiodeRepository;
+import qraps.platform.review.repository.StepDownICRepository;
+import qraps.platform.review.repository.TransistorRepository;
+import qraps.platform.utils.EntityHelper;
+import qraps.platform.utils.ParsedExcel;
+import qraps.platform.web.controller.dto.ValidateTarget;
 
+import java.io.IOException;
 import java.util.List;
-import java.util.UUID;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
 public class DirectReviewService {
 
-    private final CsvParserService csvParserService;
-    private final ReportService reportService;
+    private final ExcelParseService excelParseService;
 
     @Autowired
-    private DevMockEntityRepository devMockEntityRepository;
+    private StepDownICRepository stepdownIcRepository;
 
 
     @Autowired
-    public DirectReviewService(CsvParserService csvParserService, ReportService reportService) {
-        this.csvParserService = csvParserService;
-        this.reportService = reportService;
+    private TransistorRepository transistorRepository;
+
+
+    @Autowired
+    private DiodeRepository diodeRepository;
+
+
+    @Autowired
+    public DirectReviewService(ExcelParseService excelParseService) {
+        this.excelParseService = excelParseService;
     }
 
 
-    /**
-     * Todo: 검증 대상 분기
-     */
-    public String directReview(String target, MultipartFile file) throws Exception {
+    public ResponseReviewDto directReview(ValidateTarget target, MultipartFile file) throws IOException {
+        ParsedExcel parsedExcel = excelParseService.parseExcel(file.getInputStream());
+        String partNo = parsedExcel.getPartNo();
 
-        List<ReviewDto.CsvPositionMapper> parsedDtos = csvParserService.parseByPosition(file);
+        Object foundEntity = findTargetEntity(target, partNo);
 
-        /** Todo: DB 조회 후 DTO 검증 */
-        List<ReviewDto.Result> results =
-                parsedDtos.stream()
-                        .map(dto -> {
-                            String name = dto.getName();
-
-                            DevMockEntity foundEntity = devMockEntityRepository.findByName(name)
-                                    .orElseThrow(() -> new RuntimeException("CSV 속성 이름이 데이터베이스에 존재하지 않습니다."));
-
-                            return foundEntity.review(dto);
-                        }).collect(Collectors.toList());
-
-        /** Todo: 검증 결과 저장 */
-        String mockSavedResultId = UUID.randomUUID().toString();
-
-
-        ReportDto.Generate generateReportDto =
-                ReportDto.Generate.builder()
-                        .targetName(target)
-                        .reportId(mockSavedResultId)
-                        .build();
-
-        String generatedReportUrl = reportService.generateReport(generateReportDto, results);
-
-        /** Todo: 검증 결과 ID 반환 */
-        return generatedReportUrl;
+        List<ReviewDto.Result> reviewResults = reviewPart(parsedExcel.getParsedRows(), foundEntity);
+        return ResponseReviewDto.builder()
+                .targetName(target.getTarget())
+                .reviewResults(reviewResults)
+                .passReview(validatePassReview(reviewResults))
+                .build();
     }
+
+    private Object findTargetEntity(ValidateTarget target, String partNo) {
+        switch (target) {
+            case IC:
+                return stepdownIcRepository.findById(partNo)
+                        .orElseThrow(() -> new RuntimeException("검증 항목이 잘못되었거나 존재하지 않습니다." + partNo));
+
+            case TRANSISTOR:
+                return transistorRepository.findById(partNo)
+                        .orElseThrow(() -> new RuntimeException("검증 항목이 잘못되었거나 존재하지 않습니다." + partNo));
+
+            case DIODE:
+                return diodeRepository.findById(partNo)
+                        .orElseThrow(() -> new RuntimeException("검증 항목이 잘못되었거나 존재하지 않습니다." + partNo));
+
+            default:
+                throw new RuntimeException("검증 항목이 잘못되었거나 존재하지 않습니다." + partNo);
+        }
+    }
+
+    // 설계값과 데이터베이스에 저장된 기준 값 검증
+    private List<ReviewDto.Result> reviewPart(List<ExcelMapperDto> parsedRows, Object foundEntity) {
+        Map<String, Object> referenceValue = EntityHelper.convertEntityToMap(foundEntity);
+
+        return parsedRows.stream()
+//                .skip(1) // DB 조회용 partName 제외
+                .filter(row -> row.needValidate())
+                .map(dto -> {
+                    String partName = dto.getPartName();
+                    Double designValue = dto.getDesignValue();
+
+                    boolean validateResult = validateDesignValue(partName, referenceValue, designValue);
+                    return ReviewDto.Result.builder()
+                            .partName(dto.getPartName())
+                            .designValue(dto.getDesignValue())
+                            .passValidate(validateResult)
+                            .build();
+                })
+                .collect(Collectors.toList());
+    }
+
+    private boolean validateDesignValue(String partName, Map<String, Object> referenceValue, Double designValue) {
+        /**
+         * Todo: check floating point Double -> BigDecimal
+         * */
+        return Objects.equals(referenceValue.get(partName), designValue);
+    }
+
+    // 모든 rows 중 검증값이 하나라도 맞지 않으면 최종 검증 결과 FAIL
+    private boolean validatePassReview(List<ReviewDto.Result> reviewResults) {
+        return reviewResults.stream().allMatch(ReviewDto.Result::isPassValidate);
+    }
+
 }
