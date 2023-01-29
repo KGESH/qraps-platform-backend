3ae0edfc0c35c927e1dc633dae1bc72946b699af
KGESH <seonghyeon.jee@gmail.com>
2023-01-29 18:56:36 +0900

    Refactor: 소자 목록 리스트 조회

--- src/main/java/qraps/platform/review/repository/PartListMapperRepository.java
+++ src/main/java/qraps/platform/review/repository/PartListMapperRepository.java
@@ -3,10 +3,11 @@ package qraps.platform.review.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import qraps.platform.review.entity.PartList;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PartListMapperRepository extends JpaRepository<PartList, String> {
-    Optional<PartList> findByDevice(int device);
+    Optional<List<PartList>> findAllByDevice(int device);
 
     Optional<PartList> findByPartNo(String partNo);
 
