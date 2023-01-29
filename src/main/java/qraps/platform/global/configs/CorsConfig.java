573f6d3cf6dad13c70cd3c61f50b0f394894a2b4
KGESH <seonghyeon.jee@gmail.com>
2023-01-29 15:48:54 +0900

    Refactor: 패키지 경로 수정

--- src/main/java/qraps/platform/global/configs/CorsConfig.java
+++ src/main/java/qraps/platform/global/configs/CorsConfig.java
@@ -1,4 +1,4 @@
-package qraps.platform.configs;
+package qraps.platform.global.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
